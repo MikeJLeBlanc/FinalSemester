4d7668dcbc40f6db40fc83bdebdba5c044564f4f
Mike LeBlanc <mike_leblanc232@hotmail.com>
2024-01-05 09:31:55 -0400

    Added pay Raise and pay cheque methods to Employee class

--- C#/A1 - Classes/Employee.cs
+++ C#/A1 - Classes/Employee.cs
@@ -31,6 +31,34 @@ abstract class Employee : Person
     }
 
     public void CalculatePayRaise()
+    {
+        switch (PerformanceRating)
+        {
+            case 1:
+                Salary *= 1.05;
+                break;
+            case 2:
+                Salary *= 1.1;
+                break;
+            case 3:
+                Salary *= 1.15;
+                break;
+            case 4:
+                Salary *= 1.2;
+                break;
+            case 5:
+                Salary *= 1.25;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void CalculatePayCheque()
+    {
+        Salary /= 26;
+    }
+    
     public override string ToString()
     {
         return $"First Name: {GetFirstName()}\nLast Name: {GetLastName()}\nAddress: {GetAddress()}\nSalary: {Salary}\nPerformance Rating: {PerformanceRating}";
