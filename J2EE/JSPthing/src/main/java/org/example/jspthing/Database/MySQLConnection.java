5b8b5a0c296e64dfe7b1b6bbdaed0deead06f671
Mike LeBlanc <mike_leblanc232@hotmail.com>
2024-02-02 14:07:23 -0400

    J2EE updated | new react app started

--- J2EE/JSPthing/src/main/java/org/example/jspthing/Database/MySQLConnection.java
+++ J2EE/JSPthing/src/main/java/org/example/jspthing/Database/MySQLConnection.java
@@ -1,2 +1,15 @@
-package org.example.jspthing.Database;public class MySQLConnection {
+package org.example.jspthing.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+    private static String url = "jdbc:mysql://localhost/test";
+    private static String uname = "root";
+    private static String pass = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, uname, pass);
+    }
 }
--- J2EE/JSPthing/src/main/java/org/example/jspthing/model/user.java
+++ J2EE/JSPthing/src/main/java/org/example/jspthing/model/user.java
@@ -1,2 +1,52 @@
-package org.example.jspthing.model;public class user {
+package org.example.jspthing.model;
+
+public class user {
+
+    private int userID;
+    private String userName;
+    private String userCity;
+
+    public user(int userID, String userName, String userCity) {
+        this.userID = userID;
+        this.userName = userName;
+        this.userCity = userCity;
+    }
+
+    public user(String userName, String userCity) {
+        this.userName = userName;
+        this.userCity = userCity;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserCity() {
+        return userCity;
+    }
+
+    public void setUserCity(String userCity) {
+        this.userCity = userCity;
+    }
+
+    @Override
+    public String toString() {
+        return "user{" +
+                "userID=" + userID +
+                ", userName='" + userName + '\'' +
+                ", userCity='" + userCity + '\'' +
+                '}';
+    }
 }
