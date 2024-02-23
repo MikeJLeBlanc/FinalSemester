a20bc1c602af79c8367f796e7b2e925644039802
Mike LeBlanc <mike_leblanc232@hotmail.com>
2024-02-22 21:00:00 -0400

    finished assignment 1 - Data Structures

---
+++ Data-Structures/Assignment1/ArrayStruct.py
@@ -0,0 +1,51 @@
+#need this for type support
+import array
+
+class data:
+    #initialize itself
+    def __init__(self, arr_type):
+        self.arr_node = array.array(arr_type, [])
+    
+    #append to the end
+    def append(self, value):
+        self.arr_node.append(value)
+    
+    #append to the front of the array
+    def prepend(self, value):
+        self.arr_node.insert(0, value)
+    
+    #return the length of the array
+    def size(self):
+        return len(self.arr_node)
+    
+    #return the first element of the array
+    def head(self):
+        return self.arr_node[0]
+    
+    #return the last element of the array
+    def tail(self):
+        return self.arr_node[-1]
+    
+    #returns the element at the index provided
+    def index(self, index):
+        return self.arr_node[index]
+    
+    #remove the last element in the array
+    def pop(self):
+        self.arr_node[-1] = self.arr_node.pop()
+    
+    #returns true if the item is in the array
+    def contains(self, item):
+        return item in self.arr_node
+
+    #attempt to find item in this arra
+    def find(self, item):
+        for index, value in enumerate(self.arr_node):
+            if value == item:
+                return index, item
+            
+
+    #print everything in this array.
+    def print(self):
+        for node in self.arr_node:
+            print(node)
---
+++ Data-Structures/Assignment1/main.py
@@ -0,0 +1,50 @@
+from ArrayStruct import data
+
+## Entry into the program, also creating a data structure with type of i will create a structure for integers.
+if __name__ == '__main__':
+    array = data('i')
+
+    ## Initial append and print test, should execute in this order
+    array.append(23)
+    array.append(90)
+    array.append(34)
+    array.append(2)
+    array.append(82)
+    array.append(1)
+    array.print()
+
+    ## Print the head or first number of the array
+    print("------- Head (23)-------")
+    print(array.head())
+
+    ## Print the tail or last number of the array
+    print("------ Tail (1)--------")
+    print(array.tail())
+
+    ## print the first number of the array after we prepend a new number
+    print("------- Prepend -------")
+    array.print()
+    print("-- prepend 100 here ---")
+    array.prepend(100)
+    array.print()
+
+    ## Pop the last number and print.
+    print("------ pop --------")
+    array.pop()
+    array.print()
+
+    ## Print the size of the array.
+    print("------- Size -------")
+    print(array.size())
+
+    ## Find returns the index and item contained in the array.
+    print("------- Finds -------")
+    print(array.find(1))
+    print(array.find(100))
+    print(array.find(21))
+
+    ## prints true if the value is present, prints false if the value is missing from the array.
+    print("------ Contains --------")
+    print(array.contains(90))
+    print(array.contains(1000))
+
