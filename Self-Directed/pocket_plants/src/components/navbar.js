f6ca59af410572376b72d3e50997b09f7aa403d4
Mike LeBlanc <mike_leblanc232@hotmail.com>
2024-03-07 21:18:50 -0400

    Unit testing

---
+++ Self-Directed/pocket_plants/src/components/navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <h1>Pocket Plants</h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">New Plant</a>
+        </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- Self-Directed/pocket_plants/src/components/utilities/send-request.js
+++
@@ -1,5 +0,0 @@
-import { getToken } from './users-service';
-import axios from 'axios';
-
-export default async function sendRequest(url, method = 'GET', data = null) {
-    const options = {};}
